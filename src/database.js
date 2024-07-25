a9596755991219b5064d3ba575f05f7f427eeba9
LennynTV <lennynvega35@gmail.com>
2024-07-24 22:21:31 -0500

    Database

--- src/database.js
+++ src/database.js
@@ -2,13 +2,13 @@ import mongoose from 'mongoose'
 
 mongoose.set('strictQuery', true)
 
-const connection = async()=>{
-    try {
-        const {connection} = await mongoose.connect(process.env.MONGODB_URI_LOCAL)
-        console.log(`Database is connected on ${connection.host} - ${connection.port}`)
-    } catch (error) {
-        console.log(error);
-    }
+const connection = async () => {
+  try {
+    const { connection } = await mongoose.connect(process.env.MONGODB_URI_LOCAL)
+    console.log(`Database is connected on ${connection.host} - ${connection.port}`)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-export default  connection
+export default connection
